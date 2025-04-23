86970234926d631ff0980ae58f2b4ca80443345b
Sergii Serdiuk <serdiuk-sergii@bustour.ua>
2025-04-23 11:11:41 +0300

    add ToggleSwitch component

--- src/components/ToggleSwitch/ToggleSwitch.jsx
+++ src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -1,20 +1,24 @@
 import { useField, Formik, Form } from 'formik';
 import './ToggleSwitch.scss';
 
-const ToggleSwitch = ({ name, disabled = false }) => {
+const ToggleSwitch = ({ name, disabled = false, submitForm }) => {
   const [field, , helpers] = useField(name);
+
+  const handleChange = (e) => {
+    helpers.setValue(e.target.checked);
+    submitForm(); 
+  };
 
   return (
     <label className={`toggle-switch ${disabled ? 'disabled' : ''}`}>
       <input
         type="checkbox"
         checked={field.value}
-        onChange={(e) => helpers.setValue(e.target.checked)}
+        onChange={handleChange}
         disabled={disabled}
       />
       <span className="slider" />
     </label>
   );
 };
-
 export default ToggleSwitch;
